59aa2f6bc0a5f8e004b620ed19236dc01b33c6d5
Tiano <tianosud@gmail.com>
2025-11-01 14:46:08 +0000

    Atualização, atividade calculadora de IMC com Java Script

---
+++ index.js
@@ -0,0 +1,22 @@
+let peso = Number(prompt("Digite seu peso:"))
+let altura = Number(prompt("Digite sua altura:"))
+
+let imc = peso / (altura * altura);
+
+if (imc < 18,5) {
+
+    console.log("Abaixo do peso " + imc )
+    
+} else if (imc < 24) {
+    console.log("Peso normal" + imc)
+} else if (imc < 30 ) {
+    console.log("Sobrepeso" + imc)
+} else if (imc < 35) {
+    console.log("Obesidade grau I" + imc)
+} else if (imc < 40) {
+    console.log("Obesidade grau II" + imc)
+} else if (imc < 50) {
+    console.log("Obesidade mórbida" + imc)
+} else if (imc > 50) {
+    console.log("Rei do Ifood" + imc)
+}

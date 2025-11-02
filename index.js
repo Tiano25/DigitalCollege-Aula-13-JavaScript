13bb20a7bcba7cb039e5939cd7e4fb5938b63467
Tiano <tianosud@gmail.com>
2025-11-01 23:09:01 -0300

    Atualizar o index.js
    atualização do código

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ let altura = Number(prompt("Digite sua altura:"))
 
 let imc = ( peso / (altura * altura))
 
-if (imc < 18,5) {
+if (imc < 18.5) {
 
     console.log("Abaixo do peso")
     
 } else if (imc < 24) {
-    console.log("Peso normal")
+    console.log("Peso normal" + imc)
 } else if (imc < 30) {
-    console.log("Sobrepeso")
+    console.log("Sobrepeso" + imc)
 } else if (imc < 35) {
-    console.log("Obesidade grau I")
+    console.log("Obesidade grau I" + imc)
 } else if (imc < 40) {
-    console.log("Obesidade grau II")
+    console.log("Obesidade grau II" + imc)
 } else if (imc < 50) {
-    console.log("Obesidade mórbida")
+    console.log("Obesidade mórbida" + imc)
 } else if (imc > 50) {
-    console.log("Rei do Ifood")
+    console.log("Rei do Ifood" + imc)
 }
